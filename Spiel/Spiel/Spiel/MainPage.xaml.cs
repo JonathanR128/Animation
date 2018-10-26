97a9969cff51f66d5035e58cba8f981a4a211a57
Jonathan Rochert <jrochert@web.de>
2018-10-26 15:01:57 +0200

    Rectangle will nicht mehr 4 Argumente annehmen..

--- Spiel/Spiel/Spiel/MainPage.xaml.cs
+++ Spiel/Spiel/Spiel/MainPage.xaml.cs
@@ -13,108 +13,52 @@ namespace Spiel
         {
             InitializeComponent();
 
-
             var layout = new AbsoluteLayout();
 
             var UpperLeftBox = new BoxView { Color = Color.Blue };
             AbsoluteLayout.SetLayoutBounds(UpperLeftBox, new Rectangle(0, 0, 30, 30));
             AbsoluteLayout.SetLayoutFlags(UpperLeftBox, AbsoluteLayoutFlags.PositionProportional);
 
-            var UpperRightBox = new BoxView { Color = Color.Red };
-            AbsoluteLayout.SetLayoutBounds(UpperRightBox, new Rectangle(1, 0, 30, 30));
-            AbsoluteLayout.SetLayoutFlags(UpperRightBox, AbsoluteLayoutFlags.PositionProportional);
 
-            var LowerLeftBox = new BoxView { Color = Color.Blue };
-            AbsoluteLayout.SetLayoutBounds(LowerLeftBox, new Rectangle(0, 1, 30, 30));
-            AbsoluteLayout.SetLayoutFlags(LowerLeftBox, AbsoluteLayoutFlags.PositionProportional);
+            Layout = new AbsoluteLayout();
+            
 
-            var LowerRightBox = new BoxView { Color = Color.Blue };
-            AbsoluteLayout.SetLayoutBounds(LowerRightBox, new Rectangle(1, 1, 30, 30));
-            AbsoluteLayout.SetLayoutFlags(LowerRightBox, AbsoluteLayoutFlags.PositionProportional);
 
-            layout.Children.Add(UpperLeftBox);
-            layout.Children.Add(UpperRightBox);
-            layout.Children.Add(LowerLeftBox);
-            layout.Children.Add(LowerRightBox);
+            //double LayoutWidth = 689.5;
+            //double LayoutHeight = 386.3;
+            //double UpperRightBoxHeight = 0;
 
-            Content = layout;
-
-            double LayoutWidth = 689.5;
-            double LayoutHeight = 386.3;
-            double UpperRightBoxHeight = 0;
-            double UpperRightBoxWidth = UpperRightBox.X;
 
             //layout.SizeChanged += (sender, e) =>
             //{
             //    UpperRightBoxHeight = UpperRightBox.Y;
             //};
 
-            UpperRightBox.PropertyChanged += (sender, e) =>
-            {
-                UpperRightBoxHeight = UpperRightBox.Y;
 
-            };
-
-            UpperRightBox.TranslateTo(-200, 80 - UpperRightBoxHeight, 5000, Easing.Linear);
             //  UpperLeftBox.TranslateTo(LayoutWidth/2 - 50, LayoutHeigth/2, 3000, Easing.Linear);
 
         }
+        public Layout Layout { get; set; }
 
         //public async Task MoveRectanglesAsync()
         //{
         //    UpperRightBox.TranslateTo(-200, 80 - UpperRightBoxHeight, 5000, Easing.Linear);  
         //}
 
+        public class Rectangle : BoxView
+        {
+             BoxView UpperLeftBox = new BoxView { Color = Color.Blue };
+             
+            
+        }
 
-        //public void AbsoluteLayoutExplorationCode()
-        //{
-        //    Title = "Absolute Layout Exploration - Code";
-        //    var layout = new AbsoluteLayout();
-
-        //    var UpperLeftBox = new BoxView { Color = Color.Blue };
-        //    AbsoluteLayout.SetLayoutBounds(UpperLeftBox, new Rectangle(0, 0, 30, 30));
-        //    AbsoluteLayout.SetLayoutFlags(UpperLeftBox, AbsoluteLayoutFlags.PositionProportional);
-
-        //    var UpperRightBox = new BoxView { Color = Color.Red };
-        //    AbsoluteLayout.SetLayoutBounds(UpperRightBox, new Rectangle(1, 0, 30, 30));
-        //    AbsoluteLayout.SetLayoutFlags(UpperRightBox, AbsoluteLayoutFlags.PositionProportional);
-
-        //    var LowerLeftBox = new BoxView { Color = Color.Blue };
-        //    AbsoluteLayout.SetLayoutBounds(LowerLeftBox, new Rectangle(0, 1, 30, 30));
-        //    AbsoluteLayout.SetLayoutFlags(LowerLeftBox, AbsoluteLayoutFlags.PositionProportional);
-
-        //    var LowerRightBox = new BoxView { Color = Color.Blue };
-        //    AbsoluteLayout.SetLayoutBounds(LowerRightBox, new Rectangle(1, 1, 30, 30));
+        //AbsoluteLayout.SetLayoutBounds(LowerRightBox, new Rectangle(1, 1, 30, 30));
         //    AbsoluteLayout.SetLayoutFlags(LowerRightBox, AbsoluteLayoutFlags.PositionProportional);
-
-        //    layout.Children.Add(UpperLeftBox);
-        //    layout.Children.Add(UpperRightBox);
-        //    layout.Children.Add(LowerLeftBox);
-        //    layout.Children.Add(LowerRightBox);
-
-        //    Content = layout;
-
-        //    double LayoutWidth = 689.5;
-        //    double LayoutHeigth = 386.3;
-        //    double UpperRightBoxHeight = 0;
-        //    double UpperRightBoxWidth = UpperRightBox.X;
-
-        //    //layout.SizeChanged += (sender, e) =>
-        //    //{
-        //    //    UpperRightBoxHeight = UpperRightBox.Y;
-        //    //};
-
-        //    UpperRightBox.PropertyChanged += (sender, e) =>
-        //    {
-        //        UpperRightBoxHeight = UpperRightBox.Y;
-
-        //    };
-
-        //    UpperRightBox.TranslateTo(-200, 80 - UpperRightBoxHeight, 5000, Easing.Linear);
-        //    //  UpperLeftBox.TranslateTo(LayoutWidth/2 - 50, LayoutHeigth/2, 3000, Easing.Linear);
-
-        //}
-
+        public void Bounds(BindableObject BoxName, double xProportional, double yProportional, int xSize, int ySize)
+        {
+            AbsoluteLayout.SetLayoutBounds(BoxName, new Rectangle(xProportional, yProportional, xSize, ySize));
+            AbsoluteLayout.SetLayoutFlags(BoxName, AbsoluteLayoutFlags.PositionProportional);
+        }
 
 
     }
