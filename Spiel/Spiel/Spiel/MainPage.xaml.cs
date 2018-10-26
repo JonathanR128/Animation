78d67ec0de7760c4ddeb2d08d7d211cda912e3bf
Jonathan Rochert <jrochert@web.de>
2018-10-26 15:03:49 +0200

    Nimmt wieder 4 Argumente

--- Spiel/Spiel/Spiel/MainPage.xaml.cs
+++ Spiel/Spiel/Spiel/MainPage.xaml.cs
@@ -15,42 +15,15 @@ namespace Spiel
 
             var layout = new AbsoluteLayout();
 
-            var UpperLeftBox = new BoxView { Color = Color.Blue };
-            AbsoluteLayout.SetLayoutBounds(UpperLeftBox, new Rectangle(0, 0, 30, 30));
-            AbsoluteLayout.SetLayoutFlags(UpperLeftBox, AbsoluteLayoutFlags.PositionProportional);
+            //var UpperLeftBox = new BoxView { Color = Color.Blue };
+            //AbsoluteLayout.SetLayoutBounds(UpperLeftBox, new Rectangle(0, 0, 30, 30));
+            //AbsoluteLayout.SetLayoutFlags(UpperLeftBox, AbsoluteLayoutFlags.PositionProportional);
 
 
             Layout = new AbsoluteLayout();
-            
-
-
-            //double LayoutWidth = 689.5;
-            //double LayoutHeight = 386.3;
-            //double UpperRightBoxHeight = 0;
-
-
-            //layout.SizeChanged += (sender, e) =>
-            //{
-            //    UpperRightBoxHeight = UpperRightBox.Y;
-            //};
-
-
-            //  UpperLeftBox.TranslateTo(LayoutWidth/2 - 50, LayoutHeigth/2, 3000, Easing.Linear);
 
         }
         public Layout Layout { get; set; }
-
-        //public async Task MoveRectanglesAsync()
-        //{
-        //    UpperRightBox.TranslateTo(-200, 80 - UpperRightBoxHeight, 5000, Easing.Linear);  
-        //}
-
-        public class Rectangle : BoxView
-        {
-             BoxView UpperLeftBox = new BoxView { Color = Color.Blue };
-             
-            
-        }
 
         //AbsoluteLayout.SetLayoutBounds(LowerRightBox, new Rectangle(1, 1, 30, 30));
         //    AbsoluteLayout.SetLayoutFlags(LowerRightBox, AbsoluteLayoutFlags.PositionProportional);
