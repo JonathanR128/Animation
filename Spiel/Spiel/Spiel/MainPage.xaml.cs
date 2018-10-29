50ab7e9e481cc085e20710545818d90417c59204
Jonathan Rochert <jrochert@web.de>
2018-10-29 10:01:17 +0100

    Now it is possible to move Box2 back in origin Position

--- Spiel/Spiel/Spiel/MainPage.xaml.cs
+++ Spiel/Spiel/Spiel/MainPage.xaml.cs
@@ -12,29 +12,72 @@ namespace Spiel
         public MainPage()
         {
             InitializeComponent();
+
+
+
             MoveAsync();
+
+
+            int counterPropertyChangedBox2 = 0; 
 
 
             Box1.PropertyChanged += (sender, e) =>
             {
                 Box1Height = Box1.Y;
-                Box1Width = Box1.X;
-                
+                Box1Width = Box1.X;              
             };
+
+            Box2.PropertyChanged += (sender, e) =>
+            {
+                if (counterPropertyChangedBox2 < 5)
+                {
+                    ++counterPropertyChangedBox2;
+                    Box2StartWidth = Box2.X;
+                    Box2StartHeight = Box2.Y;
+                }
+
+                Box2Height = Box2.Y;
+                Box2Width = Box2.X;
+            };
+
+            Box3.PropertyChanged += (sender, e) =>
+            {
+                Box3Height = Box3.Y;
+                Box3Width = Box3.X;
+            };
+
+            Box4.PropertyChanged += (sender, e) =>
+            {
+                Box4Height = Box4.Y;
+                Box4Width = Box4.X;
+            };
+
         }
 
         public double Box1Width { get; set; }
         public double Box1Height { get; set; }
 
+        public double Box2Width { get; set; }
+        public double Box2Height { get; set; }
+        public double Box2StartWidth { get; set; }
+        public double Box2StartHeight { get; set; }
+
+        public double Box3Width { get; set; }
+        public double Box3Height { get; set; }
+
+        public double Box4Width { get; set; }
+        public double Box4Height { get; set; }
+
         public async Task MoveAsync()
         {
-            await MoveRectanglesAsync();
+             await MoveToCenter();
         }
 
-        public async Task MoveRectanglesAsync()
+        public async Task MoveToCenter()
         {
-           await Box1.TranslateTo(200, 80, 5000, Easing.Linear);
-            await Box1.TranslateTo(- Box1Width, - Box1Height, 5000, Easing.Linear);
+            await Box2.TranslateTo(-400, 80, 2000, Easing.Linear);
+            await Box1.TranslateTo(140, 20, 2000, Easing.Linear);
+            await Box2.TranslateTo(Box2StartWidth - Box2Width, Box2StartHeight - Box2Height, 3000, Easing.Linear); // moves box 2 back in Start Position
         }
     }
 }
