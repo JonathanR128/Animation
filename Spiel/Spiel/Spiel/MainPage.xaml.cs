e94a7c01444fdb7bc48aad77f97dc91477bef8d3
Jonathan Rochert <jrochert@web.de>
2018-11-07 10:04:53 +0100

    einiges Überflüssiges gelöscht, anderes hinzugefügt..

--- Spiel/Spiel/Spiel/MainPage.xaml.cs
+++ Spiel/Spiel/Spiel/MainPage.xaml.cs
@@ -13,61 +13,18 @@ namespace Spiel
         {
             InitializeComponent();
 
-            int counterPropertyChangedBox2 = 0;
-            int counterPropertyChangedBox3 = 0;
-            int counterPropertyChangedBox4 = 0;
-
             NameLeft = "Box1";
             NameMiddleLeft = "Box2";
             NameMiddleRight = "Box3";
             NameRight = "Box4";
+            VierPositionen = 3.5;
 
             Box4.Color = System.Drawing.Color.Purple;
 
-            MoveAsync();
-            Box1StartHeight = 0;
-            Box1StartWidth = 0;
-                       
-            Box1.PropertyChanged += (sender, e) =>
-            {
-                Box1Height = Box1.Y;
-                Box1Width = Box1.X;              
-            };
-            Box2.PropertyChanged += (sender, e) =>
-            {
-                if (counterPropertyChangedBox2 < 4)
-                {
-                    ++counterPropertyChangedBox2;
-                    Box2StartWidth = Box2.X;
-                    Box2StartHeight = Box2.Y;
-                }
-
-                Box2Height = Box2.Y;
-                Box2Width = Box2.X;
-            };
-            Box3.PropertyChanged += (sender, e) =>
-            {
-                if (counterPropertyChangedBox3 < 4)
-                {
-                    ++counterPropertyChangedBox3;
-                    Box3StartWidth = Box3.X;
-                    Box3StartHeight = Box3.Y;
-                }
-                Box3Height = Box3.Y;
-                Box3Width = Box3.X;
-            };
-            Box4.PropertyChanged += (sender, e) =>
-            {
-                if (counterPropertyChangedBox4 < 3)
-                {
-                    ++counterPropertyChangedBox4;
-                    Box4StartWidth = Box4.X;
-                    Box4StartHeight = Box4.Y;
-                }
-                Box4Height = Box4.Y;
-                Box4Width = Box4.X;
-            };
+            MoveAsync();     
+        
         }
+        public double VierPositionen { get; set; }
 
         public string NameLeft { get; set; }
         public string NameMiddleLeft { get; set; }
@@ -80,102 +37,95 @@ namespace Spiel
         public BoxView MiddleRightBox { get; set; }
         public BoxView RightBox { get; set; }
 
-        public double Box1Width { get; set; }
-        public double Box1Height { get; set; }
-        public double Box1StartWidth { get; set; }
-        public double Box1StartHeight { get; set; }
-
-        public double Box2Width { get; set; }
-        public double Box2Height { get; set; }
-        public double Box2StartWidth { get; set; }
-        public double Box2StartHeight { get; set; }
-
-        public double Box3Width { get; set; }
-        public double Box3Height { get; set; }
-        public double Box3StartWidth { get; set; }
-        public double Box3StartHeight { get; set; }
-
-        public double Box4Width { get; set; }
-        public double Box4Height { get; set; }
-        public double Box4StartWidth { get; set; }
-        public double Box4StartHeight { get; set; }
-
+      
         public async Task MoveAsync()
         {
             //Box4.BackgroundColor = System.Drawing.Color.Purple;
 
-            await Task.Delay(1200);
-            Box1.AnchorX = 1.5;
-            await Box1.RotateTo(180, 2000);
-            Box1.AnchorX = 0.5;
-            // Compensate for the change in AnchorX and AnchorY.
-            Box1.TranslationX += 80;
-            Box1.Rotation = 0;
+            //await Task.Delay(1200);
+            //Box1.AnchorX = 1.5;
+            //await Box1.RotateTo(180, 2000);
+            //Box1.AnchorX = 0.5;
+            //// Compensate for the change in AnchorX and AnchorY.
+            //Box1.TranslationX += 80;
+            //Box1.Rotation = 0;
 
+            //await Box1.TranslateTo(20, 5);
+            //Box1.AnchorX = 1.5;
+            //await Box1.RotateTo(180, 2000, Easing.Linear);
 
-            Box1.AnchorX = 1.5;
-            await Box1.RotateTo(180, 2000, Easing.Linear);
+            await MoveToCenterInRow();
+            await RotateOne();
+            await MoveBackInCorners();
 
             
 
         }
 
+        public async Task RotateOne()
+        {
+            StoreNames();
+        
+            LeftBox.AnchorX = VierPositionen;
+            await LeftBox.RotateTo(180, 3000);
+        }
+
         public async Task MoveToCenter()
         {            
-            await Box1.TranslateTo(Box4StartWidth / 2, Box4StartHeight / 2, 1000, Easing.Linear); // bewirkt nichts, weil der den ersten Befehl hier nicht packt..
+            await Box1.TranslateTo(Box4.X / 2, Box4.Y / 2, 1000, Easing.Linear); // bewirkt nichts, weil der den ersten Befehl hier nicht packt..
 
-            await Box1.TranslateTo(Box4StartWidth / 2, Box4StartHeight / 2, 2000, Easing.Linear);
-            await Box2.TranslateTo(-(Box4StartWidth / 2), Box4StartHeight / 2, 2000, Easing.Linear);
-            await Box3.TranslateTo(Box4StartWidth / 2, -(Box4StartHeight / 2), 2000, Easing.Linear);
-            await Box4.TranslateTo(-(Box4StartWidth / 2), -(Box4StartHeight / 2), 2000, Easing.Linear);
+            await Box1.TranslateTo(Box4.X / 2, Box4.Y / 2, 2000, Easing.Linear);
+            await Box2.TranslateTo(-(Box4.X / 2), Box4.Y / 2, 2000, Easing.Linear);
+            await Box3.TranslateTo(Box4.X / 2, -(Box4.Y / 2), 2000, Easing.Linear);
+            await Box4.TranslateTo(-(Box4.X / 2), -(Box4.Y / 2), 2000, Easing.Linear);
         }
 
         public async Task MoveToCenterInRow()
         {
             await Box1.TranslateTo(10, 10, 1000, Easing.Linear); // bewirkt nichts, weil der den ersten Befehl hier nicht packt..
-            await Box1.TranslateTo((Box4StartWidth / 2) - 75, Box4StartHeight / 2, 2000, Easing.Linear);
-            await Box2.TranslateTo(-(Box4StartWidth / 2) - 25, Box4StartHeight / 2, 2000, Easing.Linear);
-            await Box3.TranslateTo((Box4StartWidth / 2) + 25, -(Box4StartHeight / 2), 2000, Easing.Linear);
-            await Box4.TranslateTo(-(Box4StartWidth / 2) + 75, -(Box4StartHeight / 2), 2000, Easing.Linear);
+            await Box1.TranslateTo((Box4.X / 2) - 75, Box4.Y / 2, 2000, Easing.Linear);
+            await Box2.TranslateTo(-(Box4.X / 2) - 25, Box4.Y / 2, 2000, Easing.Linear);
+            await Box3.TranslateTo((Box4.X / 2) + 25, -(Box4.Y / 2), 2000, Easing.Linear);
+            await Box4.TranslateTo(-(Box4.X / 2) + 75, -(Box4.Y / 2), 2000, Easing.Linear);
         }
 
-        public async Task RotateOne()
-        {
-            Box1.AnchorX = 2.375;
-            Box2.AnchorX = 1.125;
-            Box3.AnchorX = -0.125;
-            Box4.AnchorX = -1.375;
-            await Task.WhenAll(
-            Box4.RotateTo(180, 3000, Easing.Linear),
-            Box3.RotateTo(180, 3000, Easing.Linear),
-            Box2.RotateTo(180, 3000, Easing.Linear),
-            Box1.RotateTo(180, 3000, Easing.Linear));
-            NameLeft = "Box4"; NameMiddleLeft = "Box3"; NameMiddleRight = "Box2"; NameRight = "Box1";
-            Box1.TranslationX = 50;
+        //public async Task RotateOne()
+        //{
+        //    Box1.AnchorX = 2.375;
+        //    Box2.AnchorX = 1.125;
+        //    Box3.AnchorX = -0.125;
+        //    Box4.AnchorX = -1.375;
+        //    await Task.WhenAll(
+        //    Box4.RotateTo(180, 3000, Easing.Linear),
+        //    Box3.RotateTo(180, 3000, Easing.Linear),
+        //    Box2.RotateTo(180, 3000, Easing.Linear),
+        //    Box1.RotateTo(180, 3000, Easing.Linear));
+        //    NameLeft = "Box4"; NameMiddleLeft = "Box3"; NameMiddleRight = "Box2"; NameRight = "Box1";
+        //    Box1.TranslationX = 50;
 
-        }
+        //}
 
-        public async Task RotateTwo()
-        {
-            Box4.AnchorX = 0.5;
-            await Box4.TranslateTo(Box4Width, Box4Height, 1);
-            await Box4.RotateTo(0, 1);
+        //public async Task RotateTwo()
+        //{
+        //    Box4.AnchorX = 0.5;
+        //    await Box4.TranslateTo(Box4Width, Box4Height, 1);
+        //    await Box4.RotateTo(0, 1);
 
 
-            Box4.AnchorX = 2.375;
-            Box3.AnchorX = 1.125;
-            Box2.AnchorX = -0.125;
-            Box1.AnchorX = -1.375;
-            // await Box4.RotateTo(180, 5000, Easing.Linear);
+        //    Box4.AnchorX = 2.375;
+        //    Box3.AnchorX = 1.125;
+        //    Box2.AnchorX = -0.125;
+        //    Box1.AnchorX = -1.375;
+        //    // await Box4.RotateTo(180, 5000, Easing.Linear);
             
-            await Task.WhenAll(
-            Box1.RotateTo(360, 3000, Easing.Linear),
-            Box2.RotateTo(360, 3000, Easing.Linear),
-            Box3.RotateTo(360, 3000, Easing.Linear),
-            Box4.RotateTo(180, 3000, Easing.Linear));
-           // NameLeft = "Box4"; NameMiddleLeft = "Box3"; NameMiddleRight = "Box2"; NameRight = "Box1";
+        //    await Task.WhenAll(
+        //    Box1.RotateTo(360, 3000, Easing.Linear),
+        //    Box2.RotateTo(360, 3000, Easing.Linear),
+        //    Box3.RotateTo(360, 3000, Easing.Linear),
+        //    Box4.RotateTo(180, 3000, Easing.Linear));
+        //   // NameLeft = "Box4"; NameMiddleLeft = "Box3"; NameMiddleRight = "Box2"; NameRight = "Box1";
 
-        }
+        //}
 
         //public async Task RotateTwo()
         //{
@@ -198,10 +148,10 @@ namespace Spiel
         public async Task MoveBackInCorners()
         {
             Box1.Color = System.Drawing.Color.Black;
-            await Box1.TranslateTo(-Box1Width, -Box1Height, 2000, Easing.Linear);                                // moves box 1 back in Start Poition 
-            await Box2.TranslateTo(Box2StartWidth - Box2Width, Box2StartHeight - Box2Height, 2000, Easing.Linear); // moves box 2 back in Start Position
-            await Box3.TranslateTo(Box3StartWidth - Box3Width, Box3StartHeight - Box3Height, 2000, Easing.Linear); // moves box 3 back in Start Position
-            await Box4.TranslateTo(Box4StartWidth - Box4Width, Box4StartHeight - Box4Height, 2000, Easing.Linear); // moves box 4 back in Start Position
+            await Box1.TranslateTo(0, 0, 2000, Easing.Linear);                                // moves box 1 back in Start Poition 
+            await Box2.TranslateTo(0, 0, 2000, Easing.Linear); // moves box 2 back in Start Position
+            await Box3.TranslateTo(0, 0, 2000, Easing.Linear); // moves box 3 back in Start Position
+            await Box4.TranslateTo(0, 0, 2000, Easing.Linear); // moves box 4 back in Start Position
         }
 
 
@@ -225,7 +175,7 @@ namespace Spiel
         //public async Task MoveToCenterInRowRightNames()
         //{
         //    StoreNames();
-        //   // await Box1.TranslateTo(Box4StartWidth / 2, Box4StartHeight / 2, 1000, Easing.Linear); // bewirkt nichts, weil der den ersten Befehl hier nicht packt..
+        //   // await Box1.TranslateTo(Box4.X / 2, Box4.Y / 2, 1000, Easing.Linear); // bewirkt nichts, weil der den ersten Befehl hier nicht packt..
         //    await LeftBox.TranslateTo(100, 100, 2000, Easing.Linear);
         //    await MiddleLeftBox.TranslateTo(-100, 100, 2000, Easing.Linear);
         //    await MiddleRightBox.TranslateTo(100, - 100, 2000, Easing.Linear);
