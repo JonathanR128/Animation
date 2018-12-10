907c91eb6efa23ba1e057bd6de2d38bdfb66fb13
Jonathan Rochert <jrochert@web.de>
2018-12-10 10:03:12 +0100

    ....  auf kommazahlen

--- ReihenfolgeVonRotationenFestlegen/ReihenfolgeVonRotationenFestlegen/Program.cs
+++ ReihenfolgeVonRotationenFestlegen/ReihenfolgeVonRotationenFestlegen/Program.cs
@@ -44,82 +44,142 @@ namespace ReihenfolgeVonRotationenFestlegen
 
         public static void Zeitrunterzaehlen()
         {
-            switch (a)
-            {
-                case 0.5:
-                    t += 5000;
-                    break;
-                case 1:
-                    t -= 500;
-                    break;
-                case 2:
-                    t -= 500;
-                    break;
-                case 3:
-                    t -= 500;
-                    break;
-                case 4:
-                    t -= 2250;
-                    break;
-                case 5:
-                    t -= 1500;
-                    break;
-                case 6:
-                    t -= 1500;
-                    break;
-                case 7:
-                    t -= 1500;
-                    break;
-                case 8:
-                    t -= 2750;
-                    break;
-                case 9:
-                    t -= 1500;
-                    break;
-                case 10:
-                    t -= 1500;
-                    break;
-                case 11:
-                    t -= 1750;
-                    break;
-                case 12:
-                    t -= 1750;
-                    break;
-                case 13:
-                    t -= 750;
-                    break;
-                case 14:
-                    t -= 500;
-                    break;
-                case 15:
-                    t -= 2250;
-                    break;
-                case 16:
-                    t -= 1750;
-                    break;
-                case 17:
-                    t -= 1750;
-                    break;
-                case 18:
-                    t -= 2000;
-                    break;
-                case 19:
-                    t -= 2250;
-                    break;
-                case 20:
-                    t -= 1500;
-                    break;
-                case 21:
-                    t -= 1250;
-                    break;
-                case 22:
-                    t -= 3000;
-                    break;
-                default:
-                    break;
 
+            
+            if (a == 1)
+            {
+                t += -500;
+            }
+            if (a == 2)
+            {
+                t += -500;
+            }
+            if (a == 3)
+            {
+                t += -500;
+            }
+            if (a == 3.1)
+            {
+                t += -500;
+            }
+            if (a == 4)
+            {
+                t += -2250;
+            }
+            if (a == 4.1)
+            {
+                t += -2250;
+            }
+            if (a == 5)
+            {
+                t += -1250;
+            }
+            if (a == 5.1)
+            {
+                t += -1500;
+            }
+            if (a == 6)
+            {
+                t += -1500;
+            }
+            if (a == 6.1)
+            {
+                t += -1750;
+            }
+            if (a == 6.2)
+            {
+                t += -1750;
+            }
+            if (a == 7)
+            {
+                t += -1750;
+            }
+            if (a == 7.1)
+            {
+                t += -1750;
+            }
+            if (a == 7.2)
+            {
+                t += -1750;
+            }
+            if (a == 7.3)
+            {
+                t += -2500;
+            }
+            if (a == 8)
+            {
+                t += -2750;
+            }
+            if (a == 8.1)
+            {
+                t += -2000;
+            }
+            if (a == 8.2)
+            {
+                t += -2250;
+            }
+            if (a == 8.3)
+            {
+                t += -3250; ;
+            }
+            if (a == 9)
+            {
+                t += -1750;
+            }
+            if (a == 9.1)
+            {
+                t += -2000;
+            }
+            if (a == 9.2)
+            {
+                t += -2250;
+            }
+            if (a == 10)
+            {
+                t += -1500;
+            }
+            if (a == 11)
+            {
+                t += -1750;
+            }
+            if (a == 11.1)
+            {
+                t += -1500;
+            }
+            if (a == 11.2)
+            {
+                t += -1250;
+            }
+            if (a == 12)
+            {
+                t += -1750;
+            }
+            if (a == 12.1)
+            {
+                t += -3000;
+            }
+            if (a == 13)
+            {
+                t += -750;
+            }
+            if (a == 13.1)
+            {
+                t += -750;
+            }
+            if (a == 14)
+            {
+                t += -750;
+            }
+            if (a == 14.1)
+            {
+                t += -750;
+            }
+            if (a == 15)
+            {
+                t += -1750;
             }
         }
+        
     }      
     
 }
