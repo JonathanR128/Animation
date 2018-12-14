38a6cf40baff97a6039e54dfce4045363d621e46
Jonathan Rochert <jrochert@web.de>
2018-12-14 09:01:05 +0100

    name wieder weg

--- Spiel/Spiel/Spiel.Android/MainActivity.cs
+++ Spiel/Spiel/Spiel.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 namespace Spiel.Droid
 {
     
-    [Activity(Name = ".MainActivity", Label = "Spiel", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Spiel", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
